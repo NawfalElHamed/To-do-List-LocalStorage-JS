a7524152a96149091e24d2e9456e32d768f13f96
NawfalElHamed <nawfelelhamed99@gmail.com>
2023-08-29 14:30:30 +0100

    first commit

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
 document.addEventListener("DOMContentLoaded", function () {
-    //
     const inputNote = document.getElementById("input-note");
     const btnAdd = document.getElementById("btn-add");
     const noteList = document.getElementById("note-list");
-
-    // Load notes from local storage
-    const notes = JSON.parse(localStorage.getItem("notes")) || [];
+    let notes = JSON.parse(localStorage.getItem("notes")) || [];
 
     // Display existing notes
     notes.forEach((note, index) => {
         createNoteElement(note.text, index);
     });
+    
 
-    // Event listener for "Add" button
     btnAdd.addEventListener("click", function () {
         const noteText = inputNote.value.trim();
         if (noteText !== "") {
-            notes.push({ text: noteText, checked: false });
-            localStorage.setItem("notes", JSON.stringify(notes));
-            const newIndex = notes.length - 1;
-            createNoteElement(noteText, newIndex);
-            inputNote.value = ""; // Clear the input field
+            const editIndex = btnAdd.dataset.editIndex;
+            if (editIndex !== undefined) {
+                // Editing existing note
+                notes[editIndex].text = noteText;
+                localStorage.setItem("notes", JSON.stringify(notes));
+                const noteDiv = document.getElementById(`note-${editIndex}`);
+                const label = noteDiv.querySelector('.form-check-label');
+                label.textContent = noteText;
+                btnAdd.textContent = "Add";
+                delete btnAdd.dataset.editIndex;
+            } else {
+                // Adding new note
+                notes.push({ text: noteText, checked: false });
+                localStorage.setItem("notes", JSON.stringify(notes));
+                createNoteElement(noteText, notes.length - 1);
+            }
+            inputNote.value = "";
         }
     });
 
-    // Function to create a note element
     function createNoteElement(noteText, index) {
         const noteDiv = document.createElement("div");
         noteDiv.className = "col";
+        noteDiv.id = `note-${index}`;
         const isChecked = notes[index].checked || false;
         noteDiv.innerHTML = `
-        <div class="alert alert-primary alert-dismissible d-flex align-items-center" role="alert">
-            <input class="form-check-input" type="checkbox" value="" id="check-${index}" ${isChecked ? "checked" : ""}>
-            <p class="m-0 ms-3">${noteText}</p>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" data-index="${index}"></button>
-        </div>
-    `;
+            <div class="alert alert-primary alert-dismissible d-flex align-items-center" role="alert">
+                <input class="form-check-input" type="checkbox" value="" id="check-${index}" ${isChecked ? "checked" : ""}>
+                <label class="form-check-label ms-2" for="check-${index}">
+                    ${noteText}
+                </label>
+                <button type="button" class="btn btn-secondary btn-edit " data-index="${index}">Edit</button>
+                <button type="button" class="btn-close" aria-label="Close" data-index="${index}"></button>
+            </div>
+        `;
+
         noteList.appendChild(noteDiv);
 
         const checkbox = noteDiv.querySelector(`#check-${index}`);
-
-        // Event listener for note checkbox
         checkbox.addEventListener("change", function () {
             notes[index].checked = checkbox.checked;
             localStorage.setItem("notes", JSON.stringify(notes));
         });
 
-        // Event listener for note delete button
-        const deleteButton = noteDiv.querySelector(`[data-index="${index}"]`);
+        const deleteButton = noteDiv.querySelector(`.btn-close`);
         deleteButton.addEventListener("click", function () {
             notes.splice(index, 1);
             localStorage.setItem("notes", JSON.stringify(notes));
             noteDiv.remove();
         });
+
+        const editButton = noteDiv.querySelector(`.btn-edit`);
+        editButton.addEventListener("click", function () {
+            // Populate the note text into the input field
+            inputNote.value = noteText;
+            // Change the "Add" button to "Update"
+            btnAdd.textContent = "Update";
+            btnAdd.dataset.editIndex = index;
+        });
     }
-
-
-})
+});
